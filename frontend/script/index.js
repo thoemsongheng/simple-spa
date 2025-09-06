04c4fc11829d204ffba5553dfb410c08978ee8a8
thoemsongheng <songhengthoem99@gmail.com>
2025-09-06 18:54:41 +0700

    feat: handling client side params

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,5 +1,21 @@
 import dashboard from "./views/dashboard.js";
 
+const regexPath = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, index) => {
+      return [key, values[index]];
+    })
+  );
+};
+
 // handling navigation without reload page helper
 const navigator = (url) => {
   history.pushState(null, null, url);
@@ -9,27 +25,27 @@ const navigator = (url) => {
 const router = async () => {
   const routes = [
     { path: "/", view: dashboard },
-    { path: "/facebook", view: () => console.log("View is loaded") },
-    { path: "/tiktok", view: () => console.log("View is loaded") },
-    { path: "/telegram", view: () => console.log("View is loaded") },
+    // { path: "/facebook", view: () => console.log("View is loaded") },
+    // { path: "/tiktok", view: () => console.log("View is loaded") },
+    // { path: "/telegram", view: () => console.log("View is loaded") },
   ];
 
   const matchRoutes = routes.map((route) => {
     return {
       route: route,
-      isMatch: location.pathname === route.path,
+      result: location.pathname.match(regexPath(route.path)),
     };
   });
 
-  let match = matchRoutes.find((matchRoute) => matchRoute.isMatch);
+  let match = matchRoutes.find((matchRoute) => matchRoute.result !== null);
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: [location.pathname],
     };
   }
 
-  const view = new match.route.view();
+  const view = new match.route.view(getParams(match));
 
   document.querySelector("#app").innerHTML = await view.getHtml();
   console.log(match);
